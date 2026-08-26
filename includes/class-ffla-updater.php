65a0636d1d019210534e8f98c1a5d43cff152a62
Ale Aruca <aruca.ale@gmail.com>
2026-08-26 13:26:41 -0400

    Align WordPress requirements in updater metadata

--- includes/class-ffla-updater.php
+++ includes/class-ffla-updater.php
@@ -380,7 +380,7 @@ class FFLA_Updater
         $info->version = $latest_version;
         $info->author = '<a href="https://github.com/' . esc_attr($this->github_user) . '">Alejandro Aruca</a>';
         $info->homepage = 'https://github.com/' . $this->github_user . '/' . $this->github_repo;
-        $info->requires = '6.0';
+        $info->requires = '6.2';
         $info->tested = '';
         $info->requires_php = '7.4';
         $info->downloaded = 0;
