b5954e66daf8e420e0091b2a22c6bc8064e081a5
Ale Aruca <aruca.ale@gmail.com>
2026-09-05 00:48:14 -0400

    Release 1.46.1

--- ffl-funnels-addons.php
+++ ffl-funnels-addons.php
@@ -3,7 +3,7 @@
  * Plugin Name:       FFL Funnels Addons
  * Plugin URI:        https://github.com/aaruca/ffl-funnels-addons
  * Description:       Modular WooCommerce toolkit for sales, checkout, tax, reviews, inventory sync, analytics, and white-label admin tools.
- * Version:           1.46.0
+ * Version:           1.46.1
  * Requires at least: 6.2
  * Requires PHP:      7.4
  * Requires Plugins:  woocommerce
@@ -20,7 +20,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Plugin constants.
-define('FFLA_VERSION', '1.46.0');
+define('FFLA_VERSION', '1.46.1');
 define('FFLA_FILE', __FILE__);
 define('FFLA_PATH', plugin_dir_path(__FILE__));
 define('FFLA_URL', plugin_dir_url(__FILE__));
